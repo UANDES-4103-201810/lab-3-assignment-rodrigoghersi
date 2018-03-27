0db514f968c57721a0316d76ce78e86c31cdd720
ConiMarchant <cbmarchant@miuandes.cl>
2018-03-27 14:28:49 -0300

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+user_list = [
+  [ "rodrigo", "ghersi", "ftrt@gmail.com",54545454,"hola","fghrthrt" ],
+ ["javier", "ghersi", "ftrt@gmail.com",54545454,"hola","fghrthrt" ],
+ ["pedro", "ghersi", "ftrt@gmail.com",54545454,"hola","fghrthrt" ],
+ ["lucas", "ghersi", "ftrt@gmail.com",54545454,"hola","fghrthrt" ],
+ ["pablito", "ghersi", "ftrt@gmail.com",54545454,"hola","fghrthrt" ]
+]
+
+
+event_list = [
+  [ "parque ohiggins", "dhgjsf", "ftgjhsdtsdth",'29/Apr/2010:06:36:15 -0700',5000],
+ ["Movistar arena", "dhgjsf", "ftgjhsdtsdth",'19/Apr/2010:06:36:15 -0700',5000],
+ ["parque ohiggins", "dhgjsf", "ftgjhsdtsdth",'30/Apr/2010:06:36:15 -0700',5000 ]
+]
+
+
+ticket_list = [
+  [ 1, 20000, 'vip'],
+ [1,10000,'x'],
+ [1,5000,'y'],
+ [ 2, 20000, 'vip'],
+ [2,10000,'x'],
+ [2,5000,'y'],
+[ 3, 20000, 'vip'],
+ [3,10000,'x'],
+ [3,5000,'y']
+]
+
+user_list.each do |name, lastname, email, phone, password, address|
+  User.create( name:name, lastname:lastname, email:email, phone:phone, password:password, address:address)
+end
+
+event_list.each do |place, address, description, startdate, capacity|
+  Event.create( place:place, address:address, description:description, startdate:startdate, capacity:capacity)
+end
+
+ticket_list.each do |event, price, type|
+  Ticket.create( event:event, price:price, type:type)
+end
+
+
+
+
+
+
+
+
+
